839a9a18b1179cc26e25b1a5ac44a73b6690f470
RNR <guzhsasha@gmail.com>
2018-11-16 19:11:37 +0200

    Add project files.

---
+++ RNR'sDiscordBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNR_sDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
