617e53bc273f004ff7efdee2b4af08811dac93e3
RNR <guzhsasha@gmail.com>
2018-11-16 21:33:28 +0200

    Realize json transfer info between prorgram and json files.In config reason

--- RNR'sDiscordBot/Program.cs
+++ RNR'sDiscordBot/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RNR_sDiscordBot
 {
@@ -10,6 +7,8 @@ namespace RNR_sDiscordBot
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Test:"+ Config.bot.token);
+            Console.Read();
         }
     }
 }
---
+++ RNR'sDiscordBot/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNR_sDiscordBot
+{
+
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+        static Utilities()
+        {
+            string jsonfile = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(jsonfile);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+    }
+}
