b05ae5bb7fdad056dc62cb57987995dae6e48695
RNR <guzhsasha@gmail.com>
2018-11-17 17:08:26 +0200

    Realize the intialization bot to servers

---
+++ RNR'sDiscordBot/CommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+
+namespace RNR_sDiscordBot
+{
+    class CommandHandler
+    {
+        DiscordSocketClient client;
+        CommandService service;
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            this.client = client;
+            service = new CommandService();
+            await service.AddModulesAsync(Assembly.GetEntryAssembly());
+            client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+            var context = new SocketCommandContext(client, msg);
+            int argPos = 0;
+            if (msg.HasStringPrefix(Config.bot.cmdPrefix, ref argPos)
+                || msg.HasMentionPrefix(client.CurrentUser, ref argPos))
+            {
+                var result = await service.ExecuteAsync(context, argPos);
+                    if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+                    
+        }
+    }
+}
--- RNR'sDiscordBot/Program.cs
+++ RNR'sDiscordBot/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
 using Newtonsoft.Json;
+using Discord;
 
 namespace RNR_sDiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        DiscordSocketClient client;
+        CommandHandler handler;
+
+        static void Main(string[] args) => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private async Task StartAsync()
         {
-            Console.WriteLine("Test:"+ Config.bot.token);
-            Console.Read();
+            if (Config.bot.token == null || Config.bot.token == "") return;
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+            client.Log += Log;
+            
+            await client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await client.StartAsync();
+            handler = new CommandHandler();
+            await handler.InitializeAsync(client);
+            await Task.Delay(-1);
+
+        }
+
+        private async Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
         }
     }
 }
